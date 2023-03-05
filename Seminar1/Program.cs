e5a7dfe76d15ba5358291c1e64f5f4c568c88bbe
Sergey Kostiv <kostiv-sergey@yandex.ru>
2023-03-05 19:44:46 +0300

    занятия семинара 1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Task 1. корень числа
+/*
+Console.Write("Input a ferst number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = number2 * number2; // квадрат числа
+
+if(number1 == quad2)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+
+// Task 2. Напишите программу, которая на входе принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N
+/*
+Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+// Task 3. 
+
+
+
+
